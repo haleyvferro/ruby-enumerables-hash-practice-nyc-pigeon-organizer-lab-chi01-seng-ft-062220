de69e2515d4626f9aed0f5c1d89214c680e85c6e
Haley Ferro <haleyvferro@gmail.com>
2020-06-18 02:21:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,6 @@ def nyc_pigeon_organizer(data)
 #  new_list = {}
 #  data_hash.each |key, value|
 #  value.each do 
- 
- hash.each_with_object({}) do |k, v|   
+
+ data.each_with_object({}) do |k, v|   
 end

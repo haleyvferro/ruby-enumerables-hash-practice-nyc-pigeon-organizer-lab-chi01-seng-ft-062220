4f526a556627281b6ea99fe192e64ef761d87483
Haley Ferro <haleyvferro@gmail.com>
2020-06-18 02:31:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,6 +13,6 @@ def nyc_pigeon_organizer(data)
        if !final_array[name][key]
          final_array[name][key] = []
        end
-       
+       final_array[name][key].push 
    final_array
 end
